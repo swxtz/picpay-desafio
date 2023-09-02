5cca8f4d3732b08121028f4b8d3b35795739007b
Gustavo Mendonça <alf4r6@gmail.com>
2023-09-02 02:00:53 -0300

    feat: create cache for email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,36 @@
-import { HttpException, Injectable } from "@nestjs/common";
+import {
+    HttpException,
+    Injectable,
+    UseInterceptors,
+    Inject,
+} from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma/prisma.service";
 import { CreateUserDto } from "./dtos/create-user.dtos";
 import { ArgonService } from "src/argon/argon/argon.service";
+import {
+    CACHE_MANAGER,
+    CacheInterceptor,
+    CacheTTL,
+} from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class UsersService {
     constructor(
         private prisma: PrismaService,
         private argon: ArgonService,
+        @Inject(CACHE_MANAGER) private cache: Cache,
     ) {}
 
+    @UseInterceptors(CacheInterceptor)
+    @CacheTTL(30)
     async create(data: CreateUserDto) {
-        const hashedPassword = await this.argon.hash(data.password);
+        const cachedData = await this.cache.get(data.email);
+
+        if (cachedData) {
+            console.log("cachedData", cachedData);
+            throw new HttpException("Email already exists", 400);
+        }
 
         const verifyEmail = await this.prisma.user.findUnique({
             where: {
@@ -19,7 +38,11 @@ export class UsersService {
             },
         });
 
+        const hashedPassword = await this.argon.hash(data.password);
+
         if (verifyEmail) {
+            console.log("aqui");
+            await this.cache.set(data.email, data.email);
             throw new HttpException("Email already exists", 400);
         }
 
