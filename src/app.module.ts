cd5a13a3bba14bf57740f6fb553e1d53c86b506d
Gustavo Mendonça <alf4r6@gmail.com>
2023-09-02 01:23:35 -0300

    feat: cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { UsersModule } from "./users/users.module";
 import { ArgonModule } from "./argon/argon.module";
+import { CacheModule } from "@nestjs/cache-manager";
 
 @Module({
-    imports: [PrismaModule, UsersModule, ArgonModule],
+    imports: [
+        PrismaModule,
+        UsersModule,
+        ArgonModule,
+        CacheModule.register({ isGlobal: true }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
